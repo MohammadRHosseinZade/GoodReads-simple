de4be03d0c65fe466565a115328a0c9095d3904a
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-12 16:44:12 +0300

    refactored models.py

--- App/DB/models.py
+++ App/DB/models.py
@@ -4,27 +4,17 @@ from passlib.context import CryptContext
 import datetime
 
 
-# Password hashing context
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(nullable=False, unique=False)
     email: str = Field(index=True, nullable=False, unique=True)
-    # created_at: datetime.datetime = Field(default_factory=datetime.datetime.now())
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.now)
     is_admin: bool = Field(default=False)
     hashed_password: str = Field(nullable=False)
 
     reviews: list["Review"] = Relationship(back_populates="owner")
     book_marked: list["Bookmark"] = Relationship(back_populates="user")
 
-    def verify_password(self, password: str) -> bool:
-        return pwd_context.verify(password, self.hashed_password)
-
-    @classmethod
-    def create(cls, username: str, email: str, password: str):
-        hashed_password = pwd_context.hash(password)
-        return cls(username=username, email=email, hashed_password=hashed_password)
     
 
 class Book(SQLModel, table=True):
