d121fc7475e356cfe957b40fb024c83ae0626c7b
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-13 17:07:59 +0300

    added jwt component

---
+++ app/core/jwt_handler.py
@@ -0,0 +1,32 @@
+import jwt
+from datetime import datetime, timedelta
+from typing import Optional
+from pydantic import BaseModel
+
+SECRET_KEY = "yNVyGX3MHyYkrbRszBuYFmpL89NI6hmc"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str    
