228decb1df1b992a544cde744cab5fa180f8618a
mohammad <mohammadrhosseinzade@gmail.com>
2024-07-13 17:11:57 +0300

    dir names changed

---
+++ app/db/database.py
@@ -0,0 +1,15 @@
+from sqlmodel import SQLModel, create_engine, Session
+from db import models
+
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_db():
+    with Session(engine) as session:
+        yield session
+
+if __name__=="__main__":
+    create_db_and_tables()
